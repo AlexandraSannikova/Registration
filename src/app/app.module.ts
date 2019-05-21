055e298eaeeec669a359bdaa75bb569556ea7734
Alexandra Sannikova <sannikovaal@mail.ru>
2019-05-21 23:17:22 +0300

    header and simple auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import {NbThemeModule, NbLayoutModule, NbButtonModule} from '@nebular/theme';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     NoopAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
+    NbThemeModule.forRoot(),
     NbLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbLayoutModule,
+    NbButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
